764c00ab77413c77930888541f12b6d6a8e14bf3
Jay Shenk <jay.shenk@gmail.com>
2018-06-24 14:37:10 -0400

    Add board class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import { Client } from 'boardgame.io/react';
 import { Game } from 'boardgame.io/core';
 
+// Return true if `cells` is in a winning configuration.
+function IsVictory(cells) {
+}
+
+// Return true if all `cells` are occupied.
+function IsDraw(cells) {
+  return cells.filter(c => c === null).length == 0;
+}
+
 const TicTacToe = Game({
   setup: () => ({ cells: Array(9).fill(null)}),
-  
+
   moves: {
     clickCell(G, ctx, id) {
       let cells = [...G.cells];
-      cells[id] = ctx.currentPlayer;
+
+      // Ensure we can't overwrite cells.
+      if (cells[id] === null) {
+        cells[id] = ctx.currentPlayer;
+      }
+
       return { ...G, cells };
+    },
+  },
+
+  flow: {
+    endGameIf: (G, ctx) => {
+      if (IsVictory(G.cells)) {
+        return { winner: ctx.currentPlayer };
+      }
+      if (IsDraw(G.cells)) {
+        return { draw: true };
+      }
     },
   },
 });
---
+++ src/board.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+class TicTacToeBoard extends React.Component {
+  onClick(id) {
+    if (this.isActive(id)) {
+      this.props.move.clickCell(id);
+      this.props.events.endTurn();
+    }
+  }
+
+  isActive(id) {
+    if (!this.props.isActive) return false;
+    if (this.props.G.cells[id] !== null) return false;
+    return true;
+  }
+
+  render() {
+    let winner = '';
+    if (this.props.ctx.gameover) {
+      winner =
+        this.props.ctx.gameover.winner !== undefined ? (
+          <div id="winner">Winner: {this.props.ctx.gameover.winner}</div>
+        ) : (
+          <div id="winner">Draw!</div>
+        );
+    }
+
+    const cellStyle = {
+      border: '1px solid #555',
+      width: '50px',
+      height: '50px',
+      lineHeight: '50px',
+      textAlign: 'center',
+    };
+
+    let rows = [];
+    for (let i = 0; i < 3; i++) {
+      let cells = [];
+      for (let j = 0; j < 3; j++) {
+        const id = 3 * i + j;
+        cells.push(
+          <td style={cellStyle} key={id} onClick={() => this.onClick(id)}>
+            {this.props.G.cells[id]}
+          </td>
+        );
+      }
+      rows.push(<tr key={i}>{cells}</tr>);
+    }
+
+    return (
+      <div>
+        <table id="board">
+          <tbody>{rows}</tbody>
+        </table>
+        {winner}
+      </div>
+    );
+  }
+}
